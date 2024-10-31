d6cd76ce104651833ac9932687c9b2fef6244ff6
chrjl <73675826+chrjl@users.noreply.github.com>
2024-10-31 15:19:58 -0700

    feat(types): CardProps

---
+++ lib/types.ts
@@ -0,0 +1,7 @@
+export interface CardProps {
+  title?: string | string[];
+  subtitle?: string | string[];
+  note?: string | string[];
+  description?: string | string[];
+  highlights?: string | string[];
+}
