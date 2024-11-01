57b7012904a0348c646f389f454121e48bfb6b1c
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-01 08:37:28 -0700

    feat: StackedCard

---
+++ lib/StackedCard.tsx
@@ -0,0 +1,69 @@
+import classnames from 'classnames';
+
+import HorizontalList from './utilities/HorizontalList';
+import LinkifiedSpan from './utilities/LinkifiedSpan';
+import type { CardProps } from './types';
+
+import styles from './StackedCard.module.css';
+
+interface Props extends CardProps {
+  className?: string;
+}
+
+export default function StackedCard({
+  title = [],
+  subtitle = [],
+  note = [],
+  description = [],
+  highlights = [],
+  className,
+}: Props) {
+  title = [title].flat().filter(Boolean);
+  subtitle = [subtitle].flat().filter(Boolean);
+  note = [note].flat().filter(Boolean);
+  description = [description].flat().filter(Boolean);
+  highlights = [highlights].flat().filter(Boolean);
+
+  return (
+    <div className={classnames(styles.container, className)}>
+      <div className={classnames(styles.heading, 'heading')}>
+        <span className={classnames(styles.title, 'title')}>
+          {title.join(' ')}
+        </span>
+        {subtitle.length ? (
+          <span className="subtitle">
+            <HorizontalList>{subtitle}</HorizontalList>
+          </span>
+        ) : null}
+      </div>
+
+      <div className={classnames(styles.body, 'body')}>
+        {description.length ? (
+          <div className="description">
+            {description.map((entry, index) => (
+              <div key={index}>{entry}</div>
+            ))}
+          </div>
+        ) : null}
+
+        {highlights.length ? (
+          <ul className={classnames(styles.highlights, 'highlights')}>
+            {highlights.map((entry, index) => (
+              <li key={index}>{entry}</li>
+            ))}
+          </ul>
+        ) : null}
+
+        {note.length ? (
+          <div className={classnames(styles.note, 'note')}>
+            <HorizontalList>
+              {note.map((entry, index) => (
+                <LinkifiedSpan key={index} value={entry} />
+              ))}
+            </HorizontalList>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,4 @@ export { default as HorizontalList } from './utilities/HorizontalList';
 export { default as LinkifiedSpan } from './utilities/LinkifiedSpan';
 export { default as HeadingCard } from './HeadingCard';
 export { default as DescriptionTable } from './DescriptionTable';
+export { default as StackedCard } from './StackedCard';
